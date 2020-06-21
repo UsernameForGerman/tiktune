db82ec62cdd35298400dac17b4870bddd1cdb7b4
German <polyakgerman@gmail.com>
2020-06-21 16:39:09 +0300

    [Fix] - bug fixed all

--- src/DAL/api/api.js
+++ src/DAL/api/api.js
@@ -6,7 +6,7 @@ import axiosRetry from "axios-retry";
 class api {
    constructor(url) {
       this.api = axios.create({
-         baseURL: "http://localhost:8000/api/v1" + url,
+         baseURL: "http://127.0.0.1:8000/api/v1" + url,
          withCredentials: true,
          headers: {
             "Content-Type": "application/json"

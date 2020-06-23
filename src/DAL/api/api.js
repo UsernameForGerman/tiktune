2ad08497dbabc329ff55b0e2ebc69e442502dabd
Whatruska <vitdub@inbox.ru>
2020-06-23 21:08:50 +0600

    [Front] Language color fix

--- src/DAL/api/api.js
+++ src/DAL/api/api.js
@@ -4,11 +4,7 @@ import "regenerator-runtime/runtime.js";
 class api {
    constructor(url) {
       this.api = axios.create({
-<<<<<<< HEAD
          baseURL: "http://localhost:8000/api/v1" + url,
-=======
-         baseURL: "http://45.143.138.48:9000/api/v1" + url,
->>>>>>> f26716b326fab797ccb9f76595810fb60140153c
          withCredentials: true,
          headers: {
             "Content-Type": "application/json"

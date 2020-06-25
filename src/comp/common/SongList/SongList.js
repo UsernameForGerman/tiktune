59c1494be2fdcdd52b7a2a901716e14d2fb81454
Whatruska <vitdub@inbox.ru>
2020-06-25 23:41:05 +0600

    [Front] Search render fix

--- src/comp/common/SongList/SongList.js
+++ src/comp/common/SongList/SongList.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./SongList.module.css"
 let SongList = (props) => {
     return(
-        <div className={classes.list} style={props.list.length === 0 ? {overflow : "hidden"} : {}}>
+        <div className={classes.list} style={props.list.length === 0 || props.result ? {overflow : "hidden"} : {}}>
             {props.list}
         </div>
     );
--- src/utils/responseFormatter.js
+++ src/utils/responseFormatter.js
@@ -3,12 +3,11 @@ import not_found from "../assets/img/not_found.png";
 let formatResponse = (resp) => {
     let formatted = {...resp};
     if (formatted.artists[0].length){
+        formatted.artists[0].name = formatted.artists[0].name.replace(";", " & ");
         formatted.artists = resp.artists.reduce((a = "", b) => {
             return a + " & " + b.name;
         })
     }
-
-    formatted.artists[0].name = formatted.artists[0].name.replace(";", " & ");
 
     if (formatted.amount >= 1000000){
         formatted.amount = Number(formatted.amount / 1000000.0) + "M"

bf3d3a7ff65276f523d18a323c91e18f771741aa
Whatruska <vitdub@inbox.ru>
2020-06-30 03:31:58 +0600

    [Front] Scroll fix

--- src/comp/common/SongList/SongList.js
+++ src/comp/common/SongList/SongList.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./SongList.module.css"
 let SongList = (props) => {
     return(
-        <div className={classes.list} style={props.list.length === 0 || props.result ? {overflow : "hidden"} : {}}>
+        <div className={classes.list}>
             {props.list}
         </div>
     );
